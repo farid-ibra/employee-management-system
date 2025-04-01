58dc1575b4bb545d63ac6940638bbf48908d779b
farid-ibra <feridibrahimli10a1@gmail.com>
2025-04-01 16:01:46 +0400

    configure routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,24 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
-import HelloWorld from './HelloWorld'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent/>
-      <ListEmployeeComponent/>
+        <Routes>
+        {/* http://localhost:3000 */}
+          <Route path='/' element= {<ListEmployeeComponent/>}></Route> 
+        {/* http://localhost:3000/employees */}
+          <Route path='/employees' element= {<ListEmployeeComponent/>}></Route> 
+        </Routes>
+      
       <FooterComponent/>
+    </BrowserRouter> 
     </>
   )
 }
