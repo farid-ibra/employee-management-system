b5c25d2c2e79cb7a0c6735f01d690479434fd1e5
farid-ibra <feridibrahimli10a1@gmail.com>
2025-04-01 15:21:05 +0400

    Added Header and Footer

---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved © 2025</span>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
